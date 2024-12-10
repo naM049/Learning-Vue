74e257035ff20b472e21792403a877fb079f0081
Naif Mbarak <nabdalla745@gmail.com>
2024-12-10 23:58:05 +0300

    Added Calculator and Accordion projects

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 
 // import TodoList from "./components/TodoList.vue"
-import RandomQuoteGenerator from "./components/RandomQuoteGenerator.vue"
-
+// import RandomQuoteGenerator from "./components/RandomQuoteGenerator.vue"
+// import AmazingCalculator from './components/AmazingCalculator.vue';
+import AccordionComponent from './components/AccordionComponent.vue';
 </script>
 
 <template>
  <!-- <TodoList /> -->
-  <RandomQuoteGenerator />
+  <!-- <RandomQuoteGenerator /> -->
+   <!-- <AmazingCalculator /> -->
+   <AccordionComponent />
 </template>
 
---
+++ src/components/AmazingCalculator.vue
@@ -0,0 +1,97 @@
+<script setup>
+import { ref } from 'vue'
+
+const displayValue = ref('0')
+
+const appendValue = (value) => {
+  if (displayValue.value === '0' && value !== ".") {
+    displayValue.value = value
+  } else {
+    displayValue.value += value
+  }
+}
+
+const clearDisplay = () => {
+  displayValue.value = '0'
+}
+
+const calculate = () => {
+  try {
+    displayValue.value = eval(displayValue.value).toString()
+  } catch (error) {
+    displayValue.value = 'Error'
+    console.error(error)
+  }
+}
+</script>
+
+
+<template>
+  <div class="calculator">
+    <input class="display" readonly v-model="displayValue">
+    <div class="buttons">
+      <button type="button" @click="appendValue('7')">7</button>
+      <button type="button" @click="appendValue('8')">8</button>
+      <button type="button" @click="appendValue('9')">9</button>
+      <button type="button" @click="appendValue('/')">/</button>
+      <button type="button" @click="appendValue('4')">4</button>
+      <button type="button" @click="appendValue('5')">5</button>
+      <button type="button" @click="appendValue('6')">6</button>
+      <button type="button" @click="appendValue('*')">*</button>
+      <button type="button" @click="appendValue('1')">1</button>
+      <button type="button" @click="appendValue('2')">2</button>
+      <button type="button" @click="appendValue('3')">3</button>
+      <button type="button" @click="appendValue('-')">-</button>
+      <button type="button" @click="appendValue('0')">0</button>
+      <button type="button" @click="appendValue('.')">.</button>
+      <button type="button" @click="calculate"> = </button>
+      <button type="button" @click="appendValue('+')">+</button>
+    </div>
+    <button type="button" class="clear-button" @click="clearDisplay">C</button>
+  </div>
+</template>
+
+
+<style scoped>
+input {
+  padding: 10px 20px;
+  margin-bottom: 20px;
+}
+
+.calculator {
+  max-width: 300px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+.display {
+  width: 100%;
+  margin-bottom: 10px;
+  padding: 10px;
+  font-size: 18px;
+  /* text-align: right; */
+}
+
+.buttons {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+}
+
+button {
+  width: 100%;
+  padding: 10px;
+  font-size: 18px;
+}
+
+.clear-button {
+  width: 100%;
+  margin-top: 10px;
+}
+
+.small-text {
+  font-size: 14px;
+}
+</style>
